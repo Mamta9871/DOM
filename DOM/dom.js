685d3876a0a045bca4d0677f7d6ecee65c5c256a
Mamta9871 <mamtapal9871@gmail.com>
2023-09-03 17:09:11 +0530

    Create nodes and Modifying Dom

--- DOM/dom.js
+++ DOM/dom.js
@@ -105,6 +105,65 @@ let itemList = document.querySelector('#items');
 // console.log(itemList.parentNode.parentNode.parentNode);
 
 // parentElement
-console.log(itemList.parentElement);
-itemList.parentElement.style.backgroundColor = '#f4f4f4';
-console.log(itemList.parentElement.parentElement.parentElement);
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentElement.parentElement.parentElement);
+
+// ChildNodes
+// console.log(itemList.childNodes);
+// console.log(itemList.children);
+// console.log(itemList.children[1]);
+// itemList.children[1].style.backgroundColor = 'yellow';
+
+// // firstChild
+// console.log(itemList.firstChild);
+// // firstElementChild
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent = 'Hello 1';
+
+// lastChild
+// console.log(itemList.lastChild);
+// // lastElementChild
+// console.log(itemList.lastElementChild);
+// itemList.lastElementChild.textContent = 'Hello 4';
+
+// nextSibling
+// console.log(itemList.nextSibling);
+// // nextElementSibling
+// console.log(itemList.nextElementSibling);
+
+// previousSibling
+// console.log(itemList.previousSibling);
+// // previousELementSibling
+// console.log(itemList.previousElementSibling);
+// itemList.previousElementSibling.style.color = 'Green';
+
+// createElement
+
+// create a div
+let newDiv = document.createElement('div');
+// add Class
+newDiv.className = 'hello';
+
+// add id
+newDiv.id = 'hello 1';
+
+// add attr
+newDiv.setAttribute('title','Hello Div');
+
+// create text node
+let newDivText = document.createTextNode('Hello World');
+
+// add text to div
+newDiv.appendChild(newDivText);
+
+let container = document.querySelector('header .container');
+let h1 = document.querySelector('header h1');
+
+console.log(newDiv);
+
+newDiv.style.fontSize = '20px'
+
+container.insertBefore(newDiv, h1);
+
+
